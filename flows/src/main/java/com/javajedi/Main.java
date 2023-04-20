e5d25374c430d1b4e31a1b286ae8c669b86e0848
baggio1103 <valijanov1103@mail.ru>
2023-04-20 22:51:15 +0300

    Implement examples on Reactive Flow introduced in Java 9

---
+++ flows/src/main/java/com/javajedi/Main.java
@@ -0,0 +1,13 @@
+package com.javajedi;
+
+import org.apache.log4j.Logger;
+
+public class Main {
+
+    private static final Logger logger = Logger.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.info("Hello world");
+        System.out.println("Hello world!");
+    }
+}
